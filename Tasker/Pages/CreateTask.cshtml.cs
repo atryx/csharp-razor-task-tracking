d73b9a9bf5e6bf128315f2fc9554ccf5a414e432
Mihai Lita <mihai.lita@btrl.ro>
2021-10-05 17:48:51 +0300

    Added DbContext

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -5,13 +5,21 @@ namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
         [BindProperty]
         public Task NewTask { get; set; }
+        public CreateTaskModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
         }
         public IActionResult OnPost()
         {
+            _context.Tasks.Add(NewTask);
+            _context.SaveChanges();
             return RedirectToPage("Index");
         }
     }
